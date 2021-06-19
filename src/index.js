6c440d52e8ebe2e310fa0d989f2f0747ec233fef
Volodymyr Ruskov <vova.ruskov@gmail.com>
2021-06-19 18:01:20 +0300

    refresh

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ import onGalleryItemClick from './js/onGalleryItemClick';
 const gallery = document.querySelector('.js-gallery');
 const modal = document.querySelector('.js-lightbox');
 const modalImage = modal.querySelector('.lightbox__image');
-// let activeImage;
 
 export { gallery, modal, modalImage };
-    
 
-
-makeMarkup(gallery,galleryItems);
+makeMarkup(gallery, galleryItems);
 gallery.addEventListener('click', onGalleryItemClick);
