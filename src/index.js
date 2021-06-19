e3b10b698f07f1a4dc002826e4c553228969b2fe
Volodymyr Ruskov <vova.ruskov@gmail.com>
2021-06-19 15:10:06 +0300

    make base code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+import galleryItems from './js/gallery-items';
+import makeMarkup from './js/markup-making';
+import onGalleryItemClick from './js/onGalleryItemClick';
+
+const gallery = document.querySelector('.js-gallery');
+const modal = document.querySelector('.js-lightbox');
+const modalImage = modal.querySelector('.lightbox__image');
+// let activeImage;
+
+export { gallery, modal, modalImage };
+    
+
+
+makeMarkup(gallery,galleryItems);
+gallery.addEventListener('click', onGalleryItemClick);
